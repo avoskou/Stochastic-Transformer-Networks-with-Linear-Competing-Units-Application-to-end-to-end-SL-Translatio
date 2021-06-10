3349c7fe1e2c4a4f68be08e14d575a865be18632
andreas <aivoskou1992@gmail.com>
2021-06-10 20:00:53 +0300

    try2

---
+++ data/download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.train"
+wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.dev"
+wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.test"
